c83a56bc845bcf5d3f8eb5eac2cbdbe71fba6ceb
felipe <felipe@midianoalvo.com.br>
2020-07-14 12:39:32 -0300

    ajustado para ser async

--- cron.js
+++ cron.js
@@ -1,12 +1,10 @@
 const cron = require('node-cron');
 console.log('CRONS ATIVADOS');
 
-const config = require('./services/config').mountConfig();
-
 const banco = require('./services/banco');
-const {  deleteCron } = config.cron;
 
-cron.schedule(deleteCron, () => {
+cron.schedule('0 1 * * 0', () => {
+    const config = require('./services/config').mountConfig();
     const mongoDb = await banco.abrirConexaoMongo(config);
     const db = await mongoDb.db(config.mongo.dbName);
     await banco.delFinished(db);
--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const config = require('./services/config').mountConfig();
-const {enterMessage, dropTemp, createRaffle, callbackMiddleware, listRaffles, deleteMessage, finishMessage } = require('./services/telegram');
+async function main() {
+    const config = require('./services/config').mountConfig();
+    const {enterMessage, dropTemp, createRaffle, callbackMiddleware, listRaffles, deleteMessage, finishMessage } = require('./services/telegram');
+    
+    const { Telegraf } = require('telegraf')
+    const session = require('telegraf/session')
+    const Stage = require('telegraf/stage')
+    const Scene = require('telegraf/scenes/base');
+    
+    const raffle = new Scene('raffle')
+    raffle.enter(enterMessage)
+    raffle.command('exit',dropTemp)
+    raffle.on('message',createRaffle)
+    
+    const stage = new Stage()
+    stage.register(raffle)
+    
+    const bot = new Telegraf(config.bot.id)
+    bot.use(session())
+    bot.use(stage.middleware())
+    bot.command('create', (ctx) => ctx.scene.enter('raffle'));
+    bot.command('finish', finishMessage);
+    bot.command('list', listRaffles);
+    bot.command('delete', deleteMessage)
+    bot.on('callback_query', callbackMiddleware);
+    bot.startPolling()
 
-const { Telegraf } = require('telegraf')
-const session = require('telegraf/session')
-const Stage = require('telegraf/stage')
-const Scene = require('telegraf/scenes/base');
+}
 
-const raffle = new Scene('raffle')
-raffle.enter(enterMessage)
-raffle.command('exit',dropTemp)
-raffle.on('message',createRaffle)
-
-const stage = new Stage()
-stage.register(raffle)
-
-const bot = new Telegraf(config.bot.id)
-bot.use(session())
-bot.use(stage.middleware())
-bot.command('create', (ctx) => ctx.scene.enter('raffle'));
-bot.command('finish', finishMessage);
-bot.command('list', listRaffles);
-bot.command('delete', deleteMessage)
-bot.on('callback_query', callbackMiddleware);
-bot.startPolling()
-
-
+main()
