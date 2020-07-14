f68deebd9a36e14f4d15b831d64d085ec966a743
felipe <felipe@midianoalvo.com.br>
2020-07-14 12:42:03 -0300

    ajustado async

--- cron.js
+++ cron.js
@@ -2,11 +2,10 @@ const cron = require('node-cron');
 console.log('CRONS ATIVADOS');
 
 const config = require('./services/config').mountConfig();
-
 const banco = require('./services/banco');
 const {  deleteCron } = config.cron;
 
-cron.schedule(deleteCron, () => {
+cron.schedule(deleteCron, async () => {
     const mongoDb = await banco.abrirConexaoMongo(config);
     const db = await mongoDb.db(config.mongo.dbName);
     await banco.delFinished(db);
