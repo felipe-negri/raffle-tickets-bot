88adbf0d84cb9d0fdd4b270437b2aaf837def6c2
felipe <felipe@midianoalvo.com.br>
2020-07-14 12:55:39 -0300

    teste pm2

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-async function main() {
-    console.log(`BOT START`)
-    const config = await require('./services/config').mountConfig();
-    const {enterMessage, dropTemp, createRaffle, callbackMiddleware, listRaffles, deleteMessage, finishMessage } = require('./services/telegram');
-    
-    const { Telegraf } = require('telegraf')
-    const session = require('telegraf/session')
-    const Stage = require('telegraf/stage')
-    const Scene = require('telegraf/scenes/base');
-    
-    const raffle = new Scene('raffle')
-    raffle.enter(enterMessage)
-    raffle.command('exit',dropTemp)
-    raffle.on('message',createRaffle)
-    
-    const stage = new Stage()
-    stage.register(raffle)
-    
-    const bot = new Telegraf(config.bot.id)
-    bot.use(session())
-    bot.use(stage.middleware())
-    bot.command('create', (ctx) => ctx.scene.enter('raffle'));
-    bot.command('finish', finishMessage);
-    bot.command('list', listRaffles);
-    bot.command('delete', deleteMessage)
-    bot.on('callback_query', callbackMiddleware);
-    bot.startPolling()
+console.log(`BOT START`)
+const config = require('./services/config').mountConfig();
+const {enterMessage, dropTemp, createRaffle, callbackMiddleware, listRaffles, deleteMessage, finishMessage } = require('./services/telegram');
 
-}
+const { Telegraf } = require('telegraf')
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Scene = require('telegraf/scenes/base');
 
-main()
+const raffle = new Scene('raffle')
+raffle.enter(enterMessage)
+raffle.command('exit',dropTemp)
+raffle.on('message',createRaffle)
+
+const stage = new Stage()
+stage.register(raffle)
+
+const bot = new Telegraf(config.bot.id)
+bot.use(session())
+bot.use(stage.middleware())
+bot.command('create', (ctx) => ctx.scene.enter('raffle'));
+bot.command('finish', finishMessage);
+bot.command('list', listRaffles);
+bot.command('delete', deleteMessage)
+bot.on('callback_query', callbackMiddleware);
+bot.launch()
+
